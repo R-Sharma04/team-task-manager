f872485623a0df81ed702303cffd925866df8c03
R-Sharma04 <risshar172004@gmail.com>
2026-05-03 00:00:29 +0530

    fix cors simple

--- team-task-manager/backend/server.js
+++ team-task-manager/backend/server.js
@@ -13,10 +13,7 @@ const cors = require("cors");
 
 app.use(cors({
   origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
